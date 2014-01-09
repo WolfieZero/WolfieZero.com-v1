2074dd1a042a362b24784bf44c9c1e0fe5124441
Neil Sweeney <neil@wolfiezero.com>
2014-01-09 17:54:46 +0000

    fixed some html validation issues;

--- content/themes/v1/layout/footer-default.php
+++ content/themes/v1/layout/footer-default.php
@@ -1,6 +1,6 @@
 
 <footer class="site-footer">
-    <p>&copy Copyright <?php echo date('Y'); ?> <a href="https://github.com/WolfieZero/WolfieZero.com">code on Github</a></p>
+    <p>&copy; Copyright <?php echo date('Y'); ?> <a href="https://github.com/WolfieZero/WolfieZero.com">code on Github</a></p>
 </footer>
 
 <hr class="top-bar">
