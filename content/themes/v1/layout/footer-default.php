d5ba5d11bab4a644cb86c0a1acd6582a758fb794
Neil Sweeney <neil@wolfiezero.com>
2014-04-07 20:55:39 +0100

    added Digital Ocean link

--- content/themes/v1/layout/footer-default.php
+++ content/themes/v1/layout/footer-default.php
@@ -1,6 +1,5 @@
-
 <footer class="site-footer" id="footer">
-    <p>&copy; Copyright <?php echo date('Y'); ?> <a href="https://github.com/WolfieZero/WolfieZero.com">code on Github</a></p>
+    <p>&copy; Copyright <?php echo date('Y'); ?> <a href="https://github.com/WolfieZero/WolfieZero.com">code on Github</a> <a href="https://www.digitalocean.com/?refcode=b09f57880a88">hosted on Digital Ocean</a></p>
 </footer>
 
 <hr class="top-bar">
